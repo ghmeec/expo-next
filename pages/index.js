230e77bec5084aaeaf256e5ea6ee6ebb33e4855c
ghmeec <georgemillanzi1234@gmail.com>
2019-12-08 10:06:41 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 // @generated: @expo/next-adapter@2.0.0-beta.9
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+      <Ionicons name="ios-mail-unread" size={32}  />
     </View>
   );
 }
