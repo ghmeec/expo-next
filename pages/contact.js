d46fa9b70a755ea2aaf538f42213c0c3929006ec
ghmeec <georgemillanzi1234@gmail.com>
2019-12-08 09:51:57 +0300

    first commit

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+export default function(){
+    return(
+        <View>
+            <Text>The Ocean</Text>
+            <Ionicons name="md-checkmark-circle" size={32} color="green" />
+        </View>
+    )
+}
